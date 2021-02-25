f9d450a174c20f9d1bd10b0ba15d8450d632ca14
YeffyCodeGit <76024824+YeffyCodeGit@users.noreply.github.com>
2021-02-25 20:48:14 +0600

    Added player movement and a broken camera controller

---
+++ MyFPSShooter/Assets/PlayerLook.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    [SerializeField] private float sensX;
+    [SerializeField] private float sensY;
+
+    Camera cam;
+
+    float mouseX;
+    float mouseY;
+
+    float multiplier = 0.01f;
+
+    float xRot;
+    float yRot;
+
+    private void Start()
+    {
+        cam = Camera.main;
+
+        cam.transform.localRotation = Quaternion.Euler(xRot, 0, 0);
+        transform.rotation = Quaternion.Euler(0, yRot, 0);
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    private void Update()
+    {
+        ProcessCameraInput();
+    }
+
+    void ProcessCameraInput()
+    {
+        mouseX = Input.GetAxisRaw("Mouse X");
+        mouseY = Input.GetAxisRaw("Mouse Y");
+
+        yRot += mouseX * sensX * multiplier;
+        xRot -= mouseY * sensY * multiplier;
+    }
+}
---
+++ MyFPSShooter/Assets/PlayerMove.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMove : MonoBehaviour
+{
+    [Header("Movement")]
+    public float moveSpeed;
+    public float moveMultiplier;
+    public float rbDrag;
+
+    float xMovement;
+    float yMovement;
+
+    Vector3 moveDir;
+
+    Rigidbody rb;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+        rb.freezeRotation = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ProcessInput();
+    }
+
+    void ProcessInput()
+    {
+        xMovement = Input.GetAxisRaw("Horizontal");
+        yMovement = Input.GetAxisRaw("Vertical");
+
+        moveDir = transform.forward * yMovement + transform.right * xMovement;
+    }
+
+    void ControlDrag()
+    {
+        rb.drag = rbDrag;
+    }
+
+    private void FixedUpdate()
+    {
+        MovePlayer();
+    }
+
+    void MovePlayer()
+    {
+        rb.AddForce(moveDir.normalized * moveSpeed, ForceMode.Acceleration);
+    }
+}
